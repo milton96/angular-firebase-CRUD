ec9652a0301d25332a8ef8dc6e3f6b605d39ff92
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2020-05-15 20:04:33 -0500

    Primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
